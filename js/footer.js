aa490a4c4b3ae10c736e75f49eef48a2366f7d0d
yutian81 <yutian88881@hotmail.com>
2025-12-11 21:10:10 +0800

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,13 +1,20 @@
 /**
  * footer.js
- * 负责在 DOM 加载完成后，将页脚的 HTML 代码插入到主要的 .container 元素之后
+ * 负责在 DOM 加载完成后，将页脚的 HTML 代码插入到主容器 .container 元素之后
  */
 
 document.addEventListener('DOMContentLoaded', function () {
+    // 获取当前年份和起始年份
+    const currentYear = new Date().getFullYear();
+    const startYear = 2025;
+    const yearText = currentYear === startYear 
+        ? `Copyright © ${startYear}`
+        : `Copyright © ${startYear}-${currentYear}`;
+    
     const htmlToInsert = `
         <div class="card-footer text-center">
             <p class="mb-0 copyright">
-                <span class="item">Copyright © 2025 Yutian81</span>
+                <span class="item">${yearText} Yutian81</span>
                 <span class="separator">|</span>
                 <a href="https://github.com/yutian81/slink/" class="item footer-link" target="_blank">
                     <i class="fa-brands fa-github me-1"></i>Github
